85b0b30590192bbc7add29a422e0632ae907beb2
Dmitriy Sinitsyn <2992274@gmail.com>
2017-07-08 22:46:56 -0600

    css+js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ var app = express();
 var fs = require('fs');
 
 
-
+const pgp = require('pg-promise')();
+const db = pgp('postgres://iphioobnwfhxqh:71052f3a32f6d245594b6e8c134f56cf4952b0e2e6838c2a7108f806437ee3a3@ec2-23-21-220-48.compute-1.amazonaws.com:5432/d2mg8u31dr7ukf');
  //var pgp = require('pg-promise');
  //var db = pgp('postgres://iphioobnwfhxqh:71052f3a32f6d245594b6e8c134f56cf4952b0e2e6838c2a7108f806437ee3a3@ec2-23-21-220-48.compute-1.amazonaws.com:5432/d2mg8u31dr7ukf');
 
