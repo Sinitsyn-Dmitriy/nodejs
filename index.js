7a097bd2de3bcfd11957a186c20cdcd73c831f25
Dmitriy Sinitsyn <2992274@gmail.com>
2017-07-08 22:36:24 -0600

    css+js

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ var fs = require('fs');
 //     console.log('ERROR:', error)
 //   });
 
+db.one('SELECT $1 AS value', 123)
+  .then(function (data) {
+    console.log('DATA:', data.value)
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  });
 
 
 
