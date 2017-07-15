8295d3701eb4f0db16e335f4923b7ed4ae9c57bd
Dmitriy Sinitsyn <2992274@gmail.com>
2017-07-15 16:10:29 -0600

    v2

--- index.js
+++ index.js
@@ -20,24 +20,17 @@ var pgp = require('pg-promise')(options);
 var pg = require("pg");
 const connectionString = "postgres://iphioobnwfhxqh:71052f3a32f6d245594b6e8c134f56cf4952b0e2e6838c2a7108f806437ee3a3@ec2-23-21-220-48.compute-1.amazonaws.com:5432/d2mg8u31dr7ukf";
 
-
-
-app.get('/getPerson', function(request, response) {
-  getPerson(request, response);
+//////////
+ToDo
+app.get('/getToDo', function(request, response) {
+  getToDo(request, response);
 });
 
-function getPerson(request, response) {
-  // First get the person's id
+function getToDo(request, response) {
+
   var id = request.query.id;
 
-  // TODO: It would be nice to check here for a valid id before continuing on...
-
-  // use a helper function to query the DB, and provide a callback for when it's done
-  getPersonFromDb(id, function(error, result) {
-    // This is the callback function that will be called when the DB is done.
-    // The job here is just to send it back.
-
-    // Make sure we got a row with the person, then prepare JSON to send back
+  getToDoFromDb(id, function(error, result) {
     if (error || result == null || result.length != 1) {
       response.status(500).json({success: false, data: error});
     } else {
@@ -47,7 +40,7 @@ function getPerson(request, response) {
   });
 }
 
-function getPersonFromDb(id, callback) {
+function getToDoFromDb(id, callback) {
   console.log("Getting person from DB with id: " + id);
 
   var client = new pg.Client(connectionString);
@@ -76,66 +69,72 @@ function getPersonFromDb(id, callback) {
 
       console.log("Found result: " + JSON.stringify(result.rows));
 
-      // call whatever function the person that called us wanted, giving it
-      // the results that we have been compiling
       callback(null, result.rows);
     });
   });
 
-} // end of getPersonFromDb
+} 
 
-// const db = pgp('postgres://iphioobnwfhxqh:71052f3a32f6d245594b6e8c134f56cf4952b0e2e6838c2a7108f806437ee3a3@ec2-23-21-220-48.compute-1.amazonaws.com:5432/d2mg8u31dr7ukf');
 
-// db.one('SELECT * FROM user_info')
-//   .then(function (data) {
-//     console.log('DATA:', data)
-//   })
-//   .catch(function (error) {
-//     console.log('ERROR:', error)
+// var pg = require("pg");
+// const connectionString = "postgres://iphioobnwfhxqh:71052f3a32f6d245594b6e8c134f56cf4952b0e2e6838c2a7108f806437ee3a3@ec2-23-21-220-48.compute-1.amazonaws.com:5432/d2mg8u31dr7ukf";
+
+// //////////
+
+// app.get('/getPerson', function(request, response) {
+//   getPerson(request, response);
+// });
+
+// function getPerson(request, response) {
+
+//   var id = request.query.id;
+
+//   getPersonFromDb(id, function(error, result) {
+//     if (error || result == null || result.length != 1) {
+//       response.status(500).json({success: false, data: error});
+//     } else {
+//       var person = result[0];
+//       response.status(200).json(result[0]);
+//     }
 //   });
+// }
 
+// function getPersonFromDb(id, callback) {
+//   console.log("Getting person from DB with id: " + id);
 
+//   var client = new pg.Client(connectionString);
 
+//   client.connect(function(err) {
+//     if (err) {
+//       console.log("Error connecting to DB: ")
+//       console.log(err);
+//       callback(err, null);
+//     }
 
+//     var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+//     var params = [id];
 
-// ==========
+//     var query = client.query(sql, params, function(err, result) {
+//       // we are now done getting the data from the DB, disconnect the client
+//       client.end(function(err) {
+//         if (err) throw err;
+//       });
 
-// var options = {  promiseLib: promise };
-// var pgp = require('pg-promise')(options);
-// var connectionString = 'postgres://iphioobnwfhxqh:71052f3a32f6d245594b6e8c134f56cf4952b0e2e6838c2a7108f806437ee3a3@ec2-23-21-220-48.compute-1.amazonaws.com:5432/d2mg8u31dr7ukf';
-// var db = pgp(connectionString);
+//       if (err) {
+//         console.log("Error in query: ")
+//         console.log(err);
+//         callback(err, null);
+//       }
 
-// module.exports = {
-//   getAllTodo: getAllTodo,
-//   getSingleTodo: getSingleTodo,
-//   createTodo: createTodo,
-//   updateTodo: updateTodo,
-//   removeTodo: removeTodo
-// };
-//===========
+//       console.log("Found result: " + JSON.stringify(result.rows));
 
-// const query = db.query('SELECT * FROM user_info ORDER BY id ASC');
-//     // Stream results back one row at a time
-//     query.on('row', (row) => {
-//       results.push(row);
+//       callback(null, result.rows);
 //     });
-
-// $usersSt = $db->prepare("SELECT * FROM user_info WHERE lastname = '". $pep ."'");  
-// $usersSt->execute();
-// $users = $usersSt->fetchAll(PDO::FETCH_ASSOC);
-
-
-////////////
-
-// db.one('SELECT $1 AS value', 123)
-//   .then(function (data) {
-//     console.log('DATA:', data.value)
-//   })
-//   .catch(function (error) {
-//     console.log('ERROR:', error)
 //   });
 
-  
+// } 
+
+///////////  
   
 
 fs.readFile('test.txt', 'utf8', function(err, data) {  
@@ -157,18 +156,7 @@ app.get('/test.txt', function(request, response) {
 
 app.use(express.static('public'));
 
-// app.get('/style.css', function(request, response) {
-//   response.send('style.css');
-// });
 
-// app.get('/style.css', function(request, response) {
-//   //response.send(cool());
-//   fs.readFile('/style.css', 'utf8', function(err, data) {  
-//     if (err) throw err;
-//     console.log(data);
-//     response.send(data);
-// });
-// });
 
 
 app.set('port', (process.env.PORT || 5123));
