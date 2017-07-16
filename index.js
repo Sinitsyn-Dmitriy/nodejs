c931f994ff25cbf63604dfccab1640336b418c3f
Dmitriy Sinitsyn <2992274@gmail.com>
2017-07-15 20:31:48 -0600

    v2

--- index.js
+++ index.js
@@ -88,7 +88,9 @@ function newToDoFromDb(id, callback) {
       callback(err, null);
     }
  
-    var sql = "SELECT id, name, descr, dline FROM todolists WHERE id = $1::int";
+//    var sql = "SELECT id, name, descr, dline FROM todolists WHERE id = $1::int";
+    var sql = "INSERT INTO todolists (name) VALUES (id = $1::int)";
+
  //   var sql = "INSERT INTO todolists (name, descr, dline) VALUES ('testName', '!!!!Play some BasketBall with friends in the park', '1017-07-15')::int";
  //   var sql = "INSERT INTO todolists (name, descr, dline) VALUES ( ${'123'}::int, ${'456'}::int, ${'1017-07-15'}::int)";
 
