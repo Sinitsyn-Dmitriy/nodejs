54033efe276f970cc8c634a9442ceeef85d58a95
Dmitriy Sinitsyn <2992274@gmail.com>
2017-07-16 14:15:44 -0600

    v2

--- index.js
+++ index.js
@@ -149,15 +149,15 @@ function updToDoFromDb(name, descr, dline, id, callback) {
     
     //var sql = "UPDATE todolists SET name='name', descr='deskript', dline='2222-07-15T00:00:00.000Z' WHERE id = $1::int";
     
-    var sql = "SELECT id, name, descr, dline FROM todolists WHERE id = $1::int";
+    //var sql = "SELECT id, name, descr, dline FROM todolists WHERE id = $1::int";
 
-    //var sql = "UPDATE todolists SET name=$1::varchar, descr='deskript', descr=$2::varchar, dline=$3::date WHERE id = $1::int";
+    var sql = "UPDATE todolists SET name=$1::varchar, descr=$2::varchar, dline=$3::date WHERE id = $1::int";
     
     //var sql = "INSERT INTO todolists (name, descr, dline) VALUES ($1::varchar, $2::varchar, $3::date)";
 
-    var params = [id]; 
+    //var params = [id]; 
     
-    //var params = [name, descr, dline, id]; 
+    var params = [name, descr, dline, id]; 
     var query = client.query(sql, params, function(err, result) {
       client.end(function(err) {
         if (err) throw err;
