dee08be31402c3ec9235f2582a30168600f1f30c
Dmitriy Sinitsyn <2992274@gmail.com>
2017-07-16 14:21:36 -0600

    v2

--- index.js
+++ index.js
@@ -146,16 +146,8 @@ function updToDoFromDb(name, descr, dline, id, callback) {
       console.log(err);
       callback(err, null);
     }  
-    
-    //var sql = "UPDATE todolists SET name='name', descr='deskript', dline='2222-07-15T00:00:00.000Z' WHERE id = $1::int";
-    
-    //var sql = "SELECT id, name, descr, dline FROM todolists WHERE id = $1::int";
 
     var sql = "UPDATE todolists SET name=$1::varchar, descr=$2::varchar, dline=$3::date WHERE id = $4::int";
-    
-    //var sql = "INSERT INTO todolists (name, descr, dline) VALUES ($1::varchar, $2::varchar, $3::date)";
-
-    //var params = [id]; 
     
     var params = [name, descr, dline, id]; 
     var query = client.query(sql, params, function(err, result) {
@@ -173,53 +165,6 @@ function updToDoFromDb(name, descr, dline, id, callback) {
   });
 } 
 
-
-
-// app.get('/updToDo', function(request, response) {
-//   updToDo(request, response);
-// });
-
-// function updToDo(request, response) {
-//   var id = request.query.id;
-//   updToDoFromDb(id, function(error, result) {
-//     if (error || result == null || result.length != 1) {
-//       response.status(500).json({success: false, data: error});
-//     } else {
-//       var todo1 = result[0];
-//       response.status(200).json(result[0]);
-//     }
-//   });
-// }
-
-// function updToDoFromDb(id, callback) {
-//   console.log("Update ToDo from DB with id: " + id);
-//   var client = new pg.Client(connectionString);
-//   client.connect(function(err) {
-//     if (err) {
-//       console.log("Error connecting to DB: ")
-//       console.log(err);
-//       callback(err, null);
-//     }
-    
- 
-//     var sql = "UPDATE todolists SET name='name', descr='deskript', dline='2222-07-15T00:00:00.000Z' WHERE id = $1::int";
-    
-
-//     var params = [id];
-//     var query = client.query(sql, params, function(err, result) {
-//       client.end(function(err) {
-//         if (err) throw err;
-//       });
-//       if (err) {
-//         console.log("Error in query: ")
-//         console.log(err);
-//         callback(err, null);
-//       }
-//       console.log("Found result: " + JSON.stringify(result.rows));
-//       callback(null, result.rows);
-//     });
-//   });
-// } 
 
 
 //delete
