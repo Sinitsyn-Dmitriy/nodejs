29d4d6833235ac1bb90b43132b7ead7d2bf6cf2e
Dmitriy Sinitsyn <2992274@gmail.com>
2017-07-15 20:39:59 -0600

    v2

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function newToDoFromDb(id, callback) {
 //    client.query('INSERT INTO todolists(text, complete) values($1, $2)', [data.text, data.complete]);
 
 //    var params = [name, descr, dline];
-    var params = [id];
+    var params = [id, id, id];
     var query = client.query(sql, params, function(err, result) {
       client.end(function(err) {
         if (err) throw err;
